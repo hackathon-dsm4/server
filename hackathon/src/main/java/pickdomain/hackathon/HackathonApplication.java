4357428f7262c95c79ceadf9cbf46cf722eb6577
도경 <dokyungkim2926@gmail.com>
2024-07-16 14:26:07 +0900

    naver

--- hackathon/src/main/java/pickdomain/hackathon/HackathonApplication.java
+++ hackathon/src/main/java/pickdomain/hackathon/HackathonApplication.java
@@ -2,6 +2,7 @@ package pickdomain.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pickdomain.hackathon.domain.news.service.NewsSerivce;
 
 @SpringBootApplication
 public class HackathonApplication {
---
+++ hackathon/src/main/java/pickdomain/hackathon/domain/news/controller/NewsController.java
@@ -0,0 +1,21 @@
+package pickdomain.hackathon.domain.news.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pickdomain.hackathon.domain.news.service.NewsSerivce;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/news")
+public class NewsController {
+    private final NewsSerivce newsSerivce;
+    @GetMapping
+    public String news(
+            @RequestParam(value = "category") String type
+    ) {
+        return newsSerivce.news(type);
+    };
+}
